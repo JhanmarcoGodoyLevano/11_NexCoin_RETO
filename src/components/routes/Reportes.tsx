ddb9f65853d5f2221310cd68b84423d7d0e85948
Jhanmarco Godoy Levano <111781171+JhanmarcoGodoyLevano@users.noreply.github.com>
2024-11-05 22:26:37 -0500

    test for sonarcloud

--- src/components/routes/Reportes.tsx
+++ src/components/routes/Reportes.tsx
@@ -5,7 +5,7 @@ const Reportes: React.FC = () => {
     <div className="flex-1 p-6 ml-64">
       <h1 className="text-3xl font-bold mb-4">Reportes</h1>
       <p>Aquí podrás ver los reportes de transacciones.</p>
-      {/* Agrega más contenido aquí según sea necesario */}
+      {/* Agrega más contenido aquí según sea necesario  */}
     </div>
   );
 };
